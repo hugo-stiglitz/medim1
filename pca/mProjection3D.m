257f08f642624b8235e53db6c98573e8b4e4948f
Lucas Dobler <lucas.dobler@gmail.com>
2015-05-06 15:40:40 +0200

    pull commit;

--- pca/mProjection3D.m
+++ pca/mProjection3D.m
@@ -14,16 +14,12 @@ for i=1 : 2
     projection2D(i,:) = EVec(i,:) * (data - m * ones(1,n));
 end
 
-EVec
-projection2D
-
 %reconstruction to 3D
 for i=1 : 2
-    recData(i,:) = EVec(i,:)' * projection2D(i,:) + m * ones(1,n);
+    recData = EVec(i,:)' * projection2D(i,:) + m * ones(1,n);
 end
 
-plot3DPCA(data', m, EVec', EVal, 1, 1);
-plot3DPCA(recData', m, EVec', EVal, 1, 1);
+plot3DPCA(data', m', EVec', EVal, 1, 1);
 
 %same as: mean(data - recData, n)
 uncertainty = sum(abs(data - recData)') / n
