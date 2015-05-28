a3778c6cf9c8c99e5010e0105dcda4ff2689b8e2
Lucas Dobler <lucas.dobler@gmail.com>
2015-05-28 16:56:22 +0200

    train;

--- pf/train.m
+++ pf/train.m
@@ -2,6 +2,23 @@ function [ rf ] = train( images, masks )
 %TRAIN Summary of this function goes here
 %   Detailed explanation goes here
 
-    rf = TreeBagger(32, features', labels', 'OOBVarImp', 'on');
+%numberOfTrees = 32;
+numberOfTrees = 10;
+
+for i = 1 : 1   %30
+    features = cache(@computeFeatures, images{i});
+    mask = masks{i};
+    labels = mask(:);
+    rf = TreeBagger(numberOfTrees, features, labels', 'OOBVarImp', 'on');
+    
+    if i == 1
+        %Aufgabe 3b
+        oobErrorRf1 = oobError(rf);
+        plot(oobErrorRf1);
+        xlabel('Number of grown trees');
+        ylabel('Out-of-bag classification error');
+    end
+end
+
 end
 
