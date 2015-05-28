490aaacd86c0eae3f53ffb7d32c5ac6fa533a420
Daniel Gehrer <daniel@xa1.at>
2015-05-28 16:56:58 +0200

    tests

--- pf/mClassification.m
+++ pf/mClassification.m
@@ -12,5 +12,13 @@ p1 = [aligned(:,1,1) , aligned(:,2,1)];
 imagesc(i1);
 axis equal;
 hold on;
-scatter(p1(:,1)+(143/2), p1(:,2)+(306/2));
+%scatter(p1(:,1)+(143/2), p1(:,2)+(306/2));
+figure();imagesc(double(i1)+double(masks{1})*100);
+axis equal;
 %plot([0 100],[0 100]);
+
+hold off;
+%figure();imagesc(masks{1});
+%figure();imagesc(masks{2});
+%figure();imagesc(masks{3});
+%figure();imagesc(masks{4});
